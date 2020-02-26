ceb052d2140d8d6612b7807bde4ade3ae98b7813
araasai <araasai@F1092-24-2112>
2020-02-26 15:20:08 -0600

    Working on PointOfSale2

---
+++ Data/IOrderItem.cs
@@ -0,0 +1,25 @@
+﻿/* Author: Abdulkareem Alkhiary
+ * Class: IOrderItem.cs 
+ * Description: Handles the items */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// An interface representing a single item in an order
+    /// </summary>
+    public interface IOrderItem
+    {
+        /// <summary>
+        /// The price of this order item
+        /// </summary>
+        public double Price { get; }
+
+        /// <summary>
+        /// The special instructions for this order item
+        /// </summary>
+        public IEnumerable<string> SpecialInstructions { get; }
+    }
+}
---
+++ Data/Order.cs
@@ -0,0 +1,36 @@
+﻿/* Author: Abdulkareem Alkhiary
+ * Class: Order.cs 
+ * Description: Handles the order */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace CowboyCafe.Data
+{
+    public class Order : INotifyPropertyChanged
+    {
+
+        private uint lastOrderNumber;
+
+        private List<IOrderItem> items;
+
+        public IEnumerable<IOrderItem> Items { get; }
+
+        public double Subtotal { get; }
+
+        public uint OrderNumber { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Add(IOrderItem item)
+        {
+
+        }
+
+        public void Remove(IOrderItem item)
+        {
+
+        }
+    }
+}
